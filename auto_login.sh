426344c6d969c71451fcdcd53529e4af46cad38d
JokerMonitor <kczhang@thoughtworks.com>
2018-07-14 09:48:04 +0800

    refactor the code

--- auto_login.sh
+++ auto_login.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 read -p "please input your username:" username
 read -s -p "please input your password:" password
-if [ -e /$username/.ssh/id_rsa.pub -a -e /$username/.ssh/id_rsa ] ;
-then 
-echo "the keys all be already ."
-else
-echo "the keys need to be generate. "
-echo "Now, generating ..."
-ssh-keygen -t rsa
-fi
 
+sync_key() {
 while read ip;
 do 
  (
-
-sshpass -p $password  ssh -o StrictHostKeyChecking=no $username@$ip "cat >> /$username/.ssh/authorized_keys" < /$username/.ssh/id_rsa.pub ;
+  sshpass -p $password  ssh -o StrictHostKeyChecking=no $username@$ip "cat >> /$username/.ssh/authorized_keys" < /$username/.ssh/id_rsa.pub ;
 #echo -e "${password}\n" | ssh-copy-id -f $username@$ip ;
 if [ $? -eq 0 ];
  then 
@@ -24,4 +16,14 @@ if [ $? -eq 0 ];
  fi
  )&
 done < servers.list
-wait
+wait;
+}
+
+if [ -e /$username/.ssh/id_rsa.pub -a -e /$username/.ssh/id_rsa ];
+then
+echo "the key is already"
+sync_key
+else
+echo "the key is not exsits . "
+exit
+fi
